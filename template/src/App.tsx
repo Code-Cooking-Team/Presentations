8523561b8a84f20410349f8167e2c2f6f226c762
Kacper Kozak <kacper.kozak@siili.com>
2020-09-02 19:48:48 +0200

    Add phenomenon

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,5 +1,21 @@
 import React from 'react'
+import { Deck, Slide } from '@phenomenon/slides'
 
 export const App = () => {
-    return <div>Test</div>
+    return (
+        <Deck>
+            <Slide>
+                <div className="App">
+                    <h1>Hello CodeSandbox</h1>
+                    <h2>Start editing to see some magic happen!</h2>
+                </div>
+            </Slide>
+            <Slide>
+                <div className="App">
+                    <h1>Hello CodeSandbox</h1>
+                    <h2>Start editing to see some magic happen!</h2>
+                </div>
+            </Slide>
+        </Deck>
+    )
 }
