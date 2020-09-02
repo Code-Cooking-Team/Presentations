28c4ac9e36a0cf85b763d2795e60ed0f8924d571
Kacper Kozak <kacper.kozak@siili.com>
2020-09-02 20:32:21 +0200

    Logo animation

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
 import { Deck, Slide } from '@phenomenon/slides'
+import { Logo } from 'components/logo/Logo'
+import React from 'react'
 
 export const App = () => {
     return (
         <Deck>
             <Slide>
                 <div className="App">
-                    <h1>Hello CodeSandbox</h1>
+                    <Logo in={0} />
                     <h2>Start editing to see some magic happen!</h2>
                 </div>
             </Slide>
---
+++ template/src/components/logo/Logo.tsx
@@ -0,0 +1,131 @@
+import { useStep } from '@phenomenon/slides'
+import { StepProps } from '@phenomenon/slides/dist/types/StepProps'
+import bezier from 'bezier-easing'
+import { delay, fromTo, htmlElementRenderer, parallel, trail, val } from 'light-trails'
+import React, { useRef } from 'react'
+import styled from 'styled-components'
+import { LogoSvg } from './LogoSvg'
+
+const easeFlip = bezier(0.34, 1.4, 0.5, 1)
+const easeFlip2 = bezier(0.34, 1.8, 0.5, 1)
+
+const timescale = 0.8
+
+export const Logo = (props: StepProps) => {
+    const ref = useRef(null)
+    const refGlass = useRef(null)
+    const refLiquid = useRef(null)
+    const refFront = useRef(null)
+    const refCooking = useRef(null)
+    const refDot = useRef(null)
+    const refBubbles = useRef(null)
+
+    useStep(
+        props.in,
+        () =>
+            parallel([
+                glass(refGlass),
+                liquid(refLiquid),
+                textFront(refFront),
+                textCooking(refCooking),
+                textDot(refDot),
+                bubbles(refBubbles),
+            ]),
+        { title: 'Logo' },
+    )
+
+    return (
+        <Container ref={ref}>
+            <LogoSvg
+                refGlass={refGlass}
+                refLiquid={refLiquid}
+                refFront={refFront}
+                refCooking={refCooking}
+                refDot={refDot}
+                refBubbles={refBubbles}
+            />
+        </Container>
+    )
+}
+
+const glass = (ref: any) =>
+    trail(htmlElementRenderer(ref.current), [
+        fromTo(
+            {
+                rotate: val(180, 0, 'deg'),
+                x: val(150, 0, 'px'),
+                opacity: val(0, 1),
+            },
+            800 / timescale,
+            easeFlip,
+        ),
+    ])
+
+const liquid = (ref: any) =>
+    trail(htmlElementRenderer(ref.current), [
+        fromTo(
+            {
+                rotate: val(-240, 0, 'deg'),
+            },
+            1000 / timescale,
+            easeFlip,
+        ),
+    ])
+
+const textFront = (ref: any) =>
+    trail(htmlElementRenderer(ref.current), [
+        delay(75 / timescale),
+        fromTo(
+            {
+                x: val(50, 0, 'px'),
+                opacity: val(0, 1),
+            },
+            900 / timescale,
+            easeFlip2,
+        ),
+    ])
+
+const textCooking = (ref: any) =>
+    trail(htmlElementRenderer(ref.current), [
+        delay(75 / timescale),
+        fromTo(
+            {
+                x: val(50, 0, 'px'),
+                opacity: val(0, 1),
+            },
+            1000 / timescale,
+            easeFlip2,
+        ),
+    ])
+
+const textDot = (ref: any) =>
+    trail(htmlElementRenderer(ref.current), [
+        delay(250 / timescale),
+        fromTo(
+            {
+                scale: val(0, 1),
+                x: val(20, 0, 'px'),
+                opacity: val(0, 1),
+            },
+            500 / timescale,
+            easeFlip2,
+        ),
+    ])
+
+const bubbles = (ref: any) =>
+    trail(htmlElementRenderer(ref.current), [
+        delay(450 / timescale),
+        fromTo(
+            {
+                opacity: val(0, 1),
+                rotate: val(-25, 0, 'deg'),
+                scale: val(0.6, 1),
+            },
+            1000 / timescale,
+            easeFlip,
+        ),
+    ])
+
+const Container = styled.div`
+    padding: 20px;
+`
