343449e3db7f3721841ad586528d5d8adc6f22ba
Kozak Bartosz <bartoszkozak94@gmail.com>
2020-09-03 18:17:19 +0200

    Add code test

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,23 +1,37 @@
-import { Deck, Slide } from '@phenomenon/slides'
+import { Deck, Slide, Frag, Code } from '@phenomenon/slides'
 import { Logo } from 'components/logo/Logo'
 import React from 'react'
 import { brandColor } from 'styles/colors'
+import styled from 'styled-components'
 
 export const App = () => {
     return (
         <Deck>
+            <Slide></Slide>
             <Slide>
-                <div className="App">
-                    <Logo color={brandColor} in={0} />
-                    <h2>Start editing to see some magic happen!</h2>
-                </div>
+                <Logo color={brandColor} in={0} />
+                <Title>Hover podkreslenie z wysuwającą się linia</Title>
             </Slide>
             <Slide>
-                <div className="App">
-                    <h1>Hello CodeSandbox</h1>
-                    <h2>Start editing to see some magic happen!</h2>
-                </div>
+                <>
+                    <Title>React</Title>
+                    <Code
+                        filename="index.html"
+                        code={`
+                            $DIV_END
+                        `}
+                    >
+                        <Frag id="DIV_END" in={2} code={`</div>`} />
+                    </Code>
+                </>
             </Slide>
         </Deck>
     )
 }
+
+const Title = styled.div`
+    text-align: center;
+    font-size: 30px;
+    font-weight: bold;
+    margin-top: 20px;
+`
